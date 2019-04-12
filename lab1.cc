dce3d697a8dbb55c4530770b520d4efbc6dd890d
Anh Leu <anhleu2@illinois.edu>
2019-04-11 19:38:21 -0500

    working basic watermark

--- lab1.cc
+++ lab1.cc
@@ -13,37 +13,31 @@ using namespace std;
 int main (int argc, char* argv[])
 {
 	// check parameters' correctness
-	if (argc != 3)
+	if (argc != 5)
 	{
-		cerr << "Usage: lab1 in.png out.png" << endl;
+		cerr << "Usage: lab1 in.png logo.png out.png alpha" << endl;
 		return 1;
 	}
+	cout << "Try: ./lab1 images/lena.png images/logo.png out.png 50 \n";
 
 	// load the imagez
 	Image image;
 	image.LoadPng (argv[1]);
+	Image logo;
+	logo.LoadPng (argv[2]);
 
-	double sum;
-	double avg;
-	for (int i = 0; i < image.Width(); i+=4){
-		for (int j = 0; j < image.Height(); j+=4){
-			sum = 0;
-			for (int x = 0; x < 4 ; x++){
-				for (int y = 0; y < 4; y++){
-					sum += image.Pixel(i+x, j+y);
-				}
-			}
-			avg = sum/16;
-			for (int x = 0; x < 4 ; x++){
-				for (int y = 0; y < 4; y++){
-					image.Pixel(i+x, j+y) = avg;
-				}
+	int alpha = atoi(argv[4]);
+
+	for (int i = 0; i < image.Width(); i++){
+		for (int j = 0; j < image.Height(); j++){
+			if (logo.Pixel(i,j) < 128){
+				image.Pixel(i,j) = 	int(image.Pixel(i,j)*(alpha/100.) + logo.Pixel(i,j)*(1-alpha/100.));
 			}
 		}
 	}
 
 	// save it
-	image.SavePng (argv[2]);
+	image.SavePng (argv[3]);
 
 	return 0;
 }
