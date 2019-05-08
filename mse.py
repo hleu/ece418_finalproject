dd294f40f5651a1f48c4591765267dc5bb218e08
Anh Leu <leuhoanganh@gmail.com>
2019-05-07 21:31:30 -0500

    add python script mse

--- mse.py
+++ mse.py
@@ -7,10 +7,10 @@ parser.add_argument('input_img2', metavar='input2', type=str)
 args = parser.parse_args()
 img1 = pyplot.imread(args.input_img1)
 img2 = pyplot.imread(args.input_img2)
-mse = 0.0
+mse = 0
 for i in range(img1.shape[0]):
 	for j in range(img1.shape[1]):
 		mse += (int(img1[i][j]*255) - int(img2[i][j]*255))* (int(img1[i][j]*255) - int(img2[i][j]*255))
 
 mse /= img1.shape[0]*img1.shape[1]
-print(mse)
+print("MSE: ",mse)
