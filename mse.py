ee990c17e6ffb110eab2c2d7555fc01b84e890a9
Anh Leu <anhleu2@illinois.edu>
2019-05-08 00:29:08 -0500

    add images for method2

--- mse.py
+++ mse.py
@@ -1,5 +1,5 @@
 import argparse
-from matplotlib import pyplot 
+from matplotlib import pyplot
 parser = argparse.ArgumentParser()
 parser.add_argument('input_img1', metavar='input1', type=str)
 parser.add_argument('input_img2', metavar='input2', type=str)
@@ -12,5 +12,5 @@ for i in range(img1.shape[0]):
 	for j in range(img1.shape[1]):
 		mse += (int(img1[i][j]*255) - int(img2[i][j]*255))* (int(img1[i][j]*255) - int(img2[i][j]*255))
 
-mse /= img1.shape[0]*img1.shape[1]
+mse /= img1.shape[0]*img1.shape[1]*100
 print("MSE: ",mse)
